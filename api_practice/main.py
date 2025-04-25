be0d90e48c4b1fe09fbedf67f7e57e556155613d
Gleb Trofimov <vivaldi@localhost.localdomain>
2025-04-24 21:59:39 -0300

    main.py fix

--- api_practice/main.py
+++ api_practice/main.py
@@ -13,7 +13,7 @@ import sklearn
 from catboost import CatBoostClassifier
 from xgboost import XGBClassifier
 import mlflow
-import mlflow.sklearn  # Если нужно логировать модели sklearn
+import mlflow.sklearn
 from fastapi import FastAPI
 import mlflow.pyfunc
 from pydantic import BaseModel
@@ -24,42 +24,26 @@ app = FastAPI()
 import mlflow
 mlflow.set_tracking_uri("http://hostname123.zapto.org:5000")
 
-logged_model = 'runs:/4c0959326a794422a6c07c0c7a16002a/model_pipeline'
+logged_model = 'runs:/4c85ecd0b1dc41f78f2d6b9dc33095cb/model_pipeline'
 
 model = mlflow.pyfunc.load_model(logged_model)
-
 
 class Df_str(BaseModel):
     data: str
     
 class Row(BaseModel):
 
-    fea_1: int
-    fea_2: float
-    fea_3: int
-    fea_4: float
-    fea_5: int
-    fea_6: int
-    fea_7: int
-    fea_8: int
-    fea_9: int
-    fea_10: int
-    fea_11: float
-    OVD_t1_mean: float
-    OVD_t1_max: int
-    OVD_t2_mean: float
-    OVD_t2_max: int
-    OVD_t3_mean: float
-    OVD_t3_max: int
-    pay_normal_mean: float
-    pay_normal_max: int
-    prod_code_median: float
-    update_date_mean: int
-    report_date_mean: int
-    prod_limit_mean: float
-    new_balance_mean: float
-    highest_balance_mean: float
-    
+    Followers: int
+    Following: int
+    Posts: int
+    Bio: int
+    profile_picture: int
+    external_link: int
+    mutual_friends: int
+    Threads: int
+    Following_Followers: float
+    Posts_Followers: float
+        
 class Dataframe(BaseModel):
     data: List[Row] = Field(..., description='')
  
